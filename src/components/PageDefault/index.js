d77bc525722acb82a9d7271ae2c234327c24c3bd
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-08-27 15:46:48 -0300

    raw: Building the project's routing system using the 'react-router-dom' library. Creating the components for the 'CadastroCategoria' and 'CadastroVideo' pages, and transforming the 'App' component into the 'Home' page. Creating a component to standardize content across all pages of the project, such as the header and footer. Finally, creating the category registration form and implementing a carousel for the videos on the home page using the 'react-slick' library

---
+++ src/components/PageDefault/index.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+import Footer from '../Footer'
+import Menu from '../Menu'
+
+const Main = styled.main`
+  background-color: var(--black);
+  color: var(--white);
+  flex: 1;
+  padding-top: 50px;
+  padding: 50px 5% 0;
+`
+
+function PageDefault({ children }) {
+  return (
+    <>
+      <Menu />
+      <Main>{children}</Main>
+      <Footer />
+    </>
+  )
+}
+
+export default PageDefault
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import './index.css'
-import App from './App'
+
+import Home from './pages/Home'
+import CadastroVideo from './pages/CadastroVideo'
+import CadastroCategoria from './pages/CadastroCategoria'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cadastro/video" element={<CadastroVideo />} />
+        <Route path="/cadastro/categoria" element={<CadastroCategoria />} />
+        <Route path="*" element={<div>Página 404</div>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
